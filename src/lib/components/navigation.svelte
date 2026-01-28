dfa71840e08899a62851f0c9e117ae862125817c
vesarautiainen <vrautiainen@gmail.com>
2026-01-28 10:44:48 +0200

    update the icon

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -62,8 +62,19 @@
 				onclick={toggleTheme}
 			>
 				{#if theme === 'dark'}
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m8.66-13.66l-.71.71M4.05 19.07l-.71.71M21 12h-1M4 12H3m16.66 5.66l-.71-.71M4.05 4.93l-.71-.71M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
-				{:else}
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					  <circle cx="12" cy="12" r="4" stroke-width="2" />
+					  <g stroke-width="2">
+						<line x1="12" y1="2" x2="12" y2="5"/>
+						<line x1="12" y1="19" x2="12" y2="22"/>
+						<line x1="2" y1="12" x2="5" y2="12"/>
+						<line x1="19" y1="12" x2="22" y2="12"/>
+						<line x1="4.22" y1="4.22" x2="6.34" y2="6.34"/>
+						<line x1="17.66" y1="17.66" x2="19.78" y2="19.78"/>
+						<line x1="4.22" y1="19.78" x2="6.34" y2="17.66"/>
+						<line x1="17.66" y1="6.34" x2="19.78" y2="4.22"/>
+					  </g>
+					</svg>				{:else}
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z" /></svg>
 				{/if}
 			</button>
